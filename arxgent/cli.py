207ecfd9600e1647366fcf47ed528e4648602ac0
mattvioli <mattvioli@gmail.com>
2026-05-24 15:49:31 +1000

    add contributing MD and fix some lint and test

--- arxgent/cli.py
+++ arxgent/cli.py
@@ -73,7 +73,7 @@ def run(date_opt: str, start: str | None, end: str | None) -> None:
         start_date = start
         end_date = end
 
-    with console.status("[bold green]Searching arxiv...") as status:
+    with console.status("[bold green]Searching arxiv..."):
         papers = research_papers(
             profile=profile,
             start_date=start_date,
